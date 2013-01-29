af4ce6587095264e4b808c689e7b190f9cf4ce87
ceednee <ceednee@gmail.com>
2013-01-29 21:00:55 +0100

    Initial commit

---
+++ tests/NkModuleInstaller/Tests/Config/ConfigTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NkModuleInstaller\Tests\Installer;
+
+use
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/NkModuleInstaller/Tests/Installer/ModuleInstallerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NkModuleInstaller\Tests\Installer;
+
+use
+
+class ModuleInstallerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
